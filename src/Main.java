c06a392309b58fe102121963f73dae6025328efa
Subhayoni Chandra <subhayoni@gmail.com>
2026-03-27 23:40:45 +0530

    Updated Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Starting Expense Manager...");
         ExpenseManager manager = new ExpenseManager();
-
+        MenuView view = new MenuView(manager);
+        view.showMenu();
     }
 }
